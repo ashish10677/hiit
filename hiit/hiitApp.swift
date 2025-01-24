ba10a5e9039bd3e29328c54c863b69f274b01b3b
Ashish Kumar Mishra <ashish10677@gmail.com>
2025-01-24 17:15:08 +0530

    Initial Commit

---
+++ hiit/hiitApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiitApp.swift
+//  hiit
+//
+//  Created by Ashish Mishra on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct hiitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hiitTests/hiitTests.swift
@@ -0,0 +1,16 @@
+//
+//  hiitTests.swift
+//  hiitTests
+//
+//  Created by Ashish Mishra on 24/01/25.
+//
+
+import Testing
+
+struct hiitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
